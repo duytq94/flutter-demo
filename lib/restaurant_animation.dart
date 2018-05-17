0ef9659800f879ae075cbedab69a4c80863089ab
Duy Tran <tranquangduy7994@gmail.com>
2018-05-17 16:04:25 +0700

    update

--- lib/restaurant_animation.dart
+++ lib/restaurant_animation.dart
@@ -21,13 +21,19 @@ class RestaurantAnimationScreen extends StatefulWidget {
 
 class RestaurantAnimationScreenState extends State<RestaurantAnimationScreen> with TickerProviderStateMixin {
 
-  AnimationController animControlPhrase1, animControlPhrase2, animControlPhrase3;
-  Animation dropDownAnimation, fadeInAnimation,
-      zoomBtnAnimation, opacityViewAnimation, opacityTextAnimation;
+  AnimationController animControlPhrase1, animControlPhrase2;
+  Animation dropDownAnimation, fadeInViewAnimation,
+      zoomBtnAnimation, fadeOutViewAnimation, fadeOutTextAnimation,
+      fadeInBottomMenuAnimation, comeUpBottomMenuAnimation
+  ;
+
   double thresholdMarginTop1 = 40.0,
       thresholdMarginTop2 = 60.0,
       thresholdSizeBtn1 = 10.0,
-      thresholdSizeBtn2 = 30.0;
+      thresholdSizeBtn2 = 30.0,
+      thresholdMarginBottomMenu1 = 30.0,
+      thresholdMarginBottomMenu2 = 30.0
+  ;
 
   bool isBtnPressed = false;
 
@@ -43,47 +49,55 @@ class RestaurantAnimationScreenState extends State<RestaurantAnimationScreen> wi
     dropDownAnimation.addListener(() {
       setState(() {});
     });
-    fadeInAnimation = new Tween(begin: 0.0, end: 1.0).animate(
+    fadeInViewAnimation = new Tween(begin: 0.0, end: 1.0).animate(
         new CurvedAnimation(parent: animControlPhrase1, curve: new Interval(0.3, 0.8, curve: Curves.easeOut)));
-    fadeInAnimation.addListener(() {
+    fadeInViewAnimation.addListener(() {
       setState(() {});
     });
 
     animControlPhrase1.forward();
 
-    // Animation phrase 2 (zoom button when be pressed)
-    animControlPhrase2 = new AnimationController(vsync: this, duration: new Duration(milliseconds: 800));
+    // Animation phrase 2 (zoom button when be pressed and show up bottom menu)
+    animControlPhrase2 = new AnimationController(vsync: this, duration: new Duration(milliseconds: 2000));
+
     zoomBtnAnimation =
         new Tween(begin: 0.0, end: 40.0).animate(
             new CurvedAnimation(parent: animControlPhrase2, curve: Curves.easeOut));
     zoomBtnAnimation.addListener(() {
       setState(() {});
     });
-    opacityViewAnimation = new Tween(begin: 1.0, end: 0.2).animate(
-        new CurvedAnimation(parent: animControlPhrase2, curve: new Interval(0.2, 0.8, curve: Curves.easeOut))
+
+    fadeOutViewAnimation = new Tween(begin: 1.0, end: 0.2).animate(
+        new CurvedAnimation(parent: animControlPhrase2, curve: new Interval(0.2, 0.8))
     );
-    opacityViewAnimation.addListener(() {
-      setState(() {});
-    });
-    opacityTextAnimation = new Tween(begin: 1.0, end: 0.0).animate(
-        new CurvedAnimation(parent: animControlPhrase2, curve: new Interval(0.2, 0.8, curve: Curves.easeOut))
-    );
-    opacityTextAnimation.addListener(() {
+    fadeOutViewAnimation.addListener(() {
       setState(() {});
     });
 
-    // Animation phrase 3 (show bottom menu)
-    animControlPhrase3 = new AnimationController(vsync: this, duration: new Duration(milliseconds: 1000));
+    fadeOutTextAnimation = new Tween(begin: 1.0, end: 0.0).animate(
+        new CurvedAnimation(parent: animControlPhrase2, curve: new Interval(0.2, 0.8))
+    );
+    fadeOutTextAnimation.addListener(() {
+      setState(() {});
+    });
+
+    fadeInBottomMenuAnimation = new Tween(begin: 0.0, end: 1.0).animate(
+        new CurvedAnimation(parent: animControlPhrase2, curve: new Interval(0.5, 1.0))
+    );
+
+    comeUpBottomMenuAnimation = new Tween(begin: 0.0, end: 40.0).animate(
+        new CurvedAnimation(parent: animControlPhrase2, curve: new Interval(0.5, 1.0))
+    );
   }
 
-  // Process value for margin top with bounce
-  double processMarginTop(double value, double thresholdValue1, thresholdValue2) {
-    if (value < thresholdValue1) {
+  // Process value for margin top with bounce for body content
+  double processMarginBodyContent(double value) {
+    if (value < thresholdMarginTop1) {
       return value;
-    } else if (value < thresholdValue2) {
-      return value = thresholdValue1 - (value - thresholdValue1);
+    } else if (value < thresholdMarginTop2) {
+      return value = thresholdMarginTop1 - (value - thresholdMarginTop1);
     } else {
-      return value = value - thresholdValue1;
+      return value = value - thresholdMarginTop1;
     }
   }
 
@@ -96,6 +110,10 @@ class RestaurantAnimationScreenState extends State<RestaurantAnimationScreen> wi
     } else {
       return value = value - (thresholdSizeBtn2 + thresholdSizeBtn1);
     }
+  }
+
+  double processMarginBottomMenu(double value) {
+    return 0.0;
   }
 
   @override
@@ -153,58 +171,52 @@ class RestaurantAnimationScreenState extends State<RestaurantAnimationScreen> wi
       ),
       decoration: new BoxDecoration(color: new Color(0xFFf53970)),
       padding: new EdgeInsets.all(10.0),
+      height: 40.0,
     );
   }
 
   Widget renderBottomMenu() {
     return new Container(
-      child: new Column(
+      child: new Row(
         children: <Widget>[
-
-          // Icons
-          new Row(
-            children: <Widget>[
-              new RawMaterialButton(
-                onPressed: () {},
-                child: new Image.asset(
-                  'images/ic_book.png', width: 40.0, height: 40.0,
-                ),
-                constraints: new BoxConstraints(),
-                highlightColor: Colors.transparent,
-                splashColor: Colors.transparent,
-              ),
-              new RawMaterialButton(
-                onPressed: () {},
-                child: new Image.asset(
-                  'images/ic_add.png', width: 40.0, height: 40.0,
-                ),
-                constraints: new BoxConstraints(),
-                highlightColor: Colors.transparent,
-                splashColor: Colors.transparent,
-              ),
-              new RawMaterialButton(
-                onPressed: () {},
-                child: new Image.asset(
-                  'images/ic_clock.png', width: 40.0, height: 40.0,
-                ),
-                constraints: new BoxConstraints(),
-                highlightColor: Colors.transparent,
-                splashColor: Colors.transparent,
-              ),
-              new RawMaterialButton(
-                onPressed: () {},
-                child: new Image.asset(
-                  'images/ic_cancel.png', width: 40.0, height: 40.0,
-                ),
-                constraints: new BoxConstraints(),
-                highlightColor: Colors.transparent,
-                splashColor: Colors.transparent,
-              ),
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          new RawMaterialButton(
+            onPressed: () {},
+            child: new Image.asset(
+              'images/ic_book.png', width: 40.0, height: 40.0,
+            ),
+            constraints: new BoxConstraints(),
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+          ),
+          new RawMaterialButton(
+            onPressed: () {},
+            child: new Image.asset(
+              'images/ic_add.png', width: 40.0, height: 40.0,
+            ),
+            constraints: new BoxConstraints(),
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+          ),
+          new RawMaterialButton(
+            onPressed: () {},
+            child: new Image.asset(
+              'images/ic_clock.png', width: 40.0, height: 40.0,
+            ),
+            constraints: new BoxConstraints(),
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+          ),
+          new RawMaterialButton(
+            onPressed: () {},
+            child: new Image.asset(
+              'images/ic_cancel.png', width: 40.0, height: 40.0,
+            ),
+            constraints: new BoxConstraints(),
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
           ),
         ],
-
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       ),
     );
   }
@@ -426,103 +438,111 @@ class RestaurantAnimationScreenState extends State<RestaurantAnimationScreen> wi
   @override
   Widget build(BuildContext context) {
     return new Container(
+      width: double.infinity,
+      height: double.infinity,
       decoration: new BoxDecoration(color: new Color(0xfff8f9fb)),
-      child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+      child: new Stack(
+        children: <Widget>[
 
-            // Tab menu
-            renderTabMenu(),
+          // Tab menu
+          renderTabMenu(),
 
-            // Main content
-            new Opacity(
-              opacity: fadeInAnimation.value,
-              child: new Container(
-                child: new Column(
-                  children: <Widget>[
+          // Body content
+          new Positioned(
+            top: 40.0 + processMarginBodyContent(dropDownAnimation.value),
+            left: 0.0,
+            right: 0.0,
+            child: new Opacity(
+              opacity: fadeInViewAnimation.value,
+              child: new Column(
+                children: <Widget>[
 
-                    // Text entrance
-                    new Center(
-                      child: new Opacity(
-                        child: new Padding(
-                          padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-                          child: new Text(
-                            'ENTRANCE',
-                            style: new TextStyle(color: new Color(0xFF575869), fontWeight: FontWeight.bold,),
-                          ),
+                  // Text entrance
+                  new Center(
+                    child: new Opacity(
+                      child: new Padding(
+                        padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+                        child: new Text(
+                          'ENTRANCE',
+                          style: new TextStyle(color: new Color(0xFF575869), fontWeight: FontWeight.bold,),
                         ),
-                        opacity: opacityTextAnimation.value,
                       ),
+                      opacity: fadeOutTextAnimation.value,
                     ),
+                  ),
 
-                    // Group tables
-                    new Column(
-                      children: <Widget>[
+                  // Group tables
+                  new Column(
+                    children: <Widget>[
 
-                        // Big table
-                        new Container(
+                      // Big table
+                      new Container(
+                        child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            new Container(
+                                child: renderBigTable2('green', onBtnPressed), width: 120.0
+                            ),
+                            new Opacity(
+                                child: renderBigTable('pink', null),
+                                opacity: fadeOutViewAnimation.value
+                            ),
+                          ],
+                        ),
+                        margin: new EdgeInsets.only(left: 10.0, right: 10.0),
+                        height: 120.0,
+                      ),
+
+                      // Small table
+                      new Container(
+                        child: new Opacity(
                           child: new Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: <Widget>[
-                              new Container(
-                                  child: renderBigTable2('green', onBtnPressed), width: 120.0
-                              ),
-                              new Opacity(
-                                  child: renderBigTable('pink', null),
-                                  opacity: opacityViewAnimation.value
-                              ),
+                              renderSmallTable('green', null),
+                              renderSmallTable('yellow', null),
+                              renderSmallTable('green', null),
                             ],
                           ),
-                          margin: new EdgeInsets.only(left: 10.0, right: 10.0),
-                          height: 120.0,
+                          opacity: fadeOutViewAnimation.value,
                         ),
+                        margin: new EdgeInsets.only(left: 20.0, right: 20.0),
+                        height: 100.0,
+                      ),
 
-                        // Small table
-                        new Container(
-                          child: new Opacity(
-                            child: new Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                renderSmallTable('green', null),
-                                renderSmallTable('yellow', null),
-                                renderSmallTable('green', null),
-                              ],
-                            ),
-                            opacity: opacityViewAnimation.value,
+                      // Big table
+                      new Container(
+                        child: new Opacity(
+                          child: new Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              renderBigTable('pink', null),
+                              renderBigTable('green', null),
+                            ],
                           ),
-                          margin: new EdgeInsets.only(left: 20.0, right: 20.0),
-                          height: 100.0,
+                          opacity: fadeOutViewAnimation.value,
                         ),
-
-                        // Big table
-                        new Container(
-                          child: new Opacity(
-                            child: new Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                renderBigTable('pink', null),
-                                renderBigTable('green', null),
-                              ],
-                            ),
-                            opacity: opacityViewAnimation.value,
-                          ),
-                          margin: new EdgeInsets.only(left: 10.0, right: 10.0),
-                          height: 120.0,
-                        ),
-
-                      ],
-                    ),
-                  ],
-                ),
-                margin: new EdgeInsets.only(
-                    top: processMarginTop(dropDownAnimation.value, thresholdMarginTop1, thresholdMarginTop2)
-                ),
+                        margin: new EdgeInsets.only(left: 10.0, right: 10.0),
+                        height: 120.0,
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
+          ),
 
-            // bottom menu
-            renderBottomMenu()
-          ]
+          // Bottom menu
+          new Positioned(
+            child: new Opacity(
+                opacity: fadeInBottomMenuAnimation.value,
+                child: renderBottomMenu()
+            ),
+            bottom: 10.0,
+            left: 0.0,
+            right: 0.0,
+          ),
+        ],
       ),
     );
   }
