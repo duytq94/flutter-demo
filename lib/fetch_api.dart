fa6e2dca60a990afb6f0ae2bd9d095c1f8ed2377
Duy Tran <tranquangduy7994@gmail.com>
2018-05-25 11:46:12 +0700

    update

--- lib/fetch_api.dart
+++ lib/fetch_api.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -25,39 +25,69 @@ class FetchApiScreen extends StatefulWidget {
 class FetchApiScreenState extends State<FetchApiScreen> {
   List data;
 
-  Future<void> fetchPost() async {
+  Future<Null> fetchData() async {
     final response = await http.get('https://api.stackexchange.com/2.2/questions?sort=activity&site=stackoverflow');
     this.setState(() {
       data = json.decode(response.body)['items'];
     });
   }
 
+  void openBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    fetchPost();
+    fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(itemBuilder: (BuildContext context, int index) {
-      return new Card(
-        margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
-        child: new Row(
-          children: <Widget>[
-            new Container(
-              decoration: new BoxDecoration(
-                  image: new DecorationImage(
-                    image: new NetworkImage(data[index]['owner']['profile_image']),
-                  ),
-                  borderRadius: new BorderRadius.all(new Radius.circular(50.0))),
-            ),
-            new Column(
-              children: <Widget>[new Text(data[index]['title'])],
-            )
-          ],
+    if (data != null) {
+      return new RefreshIndicator(
+        child: new ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return new Card(
+              margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
+              child: new FlatButton(
+                onPressed: () => openBrowser(data[index]['link']),
+                padding: new EdgeInsets.all(0.0),
+                child: new Row(
+                  children: <Widget>[
+                    new Container(
+                      width: 40.0,
+                      height: 40.0,
+                      margin: new EdgeInsets.all(10.0),
+                      decoration: new BoxDecoration(
+                          image: new DecorationImage(
+                              image: new NetworkImage(data[index]['owner']['profile_image']), fit: BoxFit.cover),
+                          borderRadius: new BorderRadius.all(new Radius.circular(20.0))),
+                    ),
+                    new Flexible(
+                        child: new Container(
+                      child: new Text(
+                        data[index]['title'],
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      margin: new EdgeInsets.all(10.0),
+                    )),
+                  ],
+                ),
+              ),
+            );
+          },
+          padding: new EdgeInsets.only(top: 10.0, bottom: 10.0),
         ),
+        onRefresh: fetchData,
       );
-    });
+    } else {
+      return new Center(child: new CircularProgressIndicator());
+    }
   }
 }
